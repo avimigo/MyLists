99b6457ad2cf3c1363407acfcfa40f4a41ad39b6
Migo <>
2022-04-27 01:18:09 +0900

    feat: 入力フォーム、ボタンのクリックイベント追加

--- my-lists/src/components/MyProteinTable.vue
+++ my-lists/src/components/MyProteinTable.vue
@@ -1,4 +1,21 @@
 <template>
+  <div class="mb-4">
+    <input
+      type="text"
+      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      id="title"
+      placeholder="Title"
+    />
+  </div>
+  <div class="flex items-center justify-between mb-4">
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      type="button"
+      v-on:click="greet"
+    >
+      Add
+    </button>
+  </div>
   <table>
     <thead>
       <tr class="bg-gray-100">
@@ -36,6 +53,11 @@ export default {
         },
       ],
     };
+  },
+  methods: {
+    greet() {
+      alert("Hello!");
+    },
   },
 };
 </script>
