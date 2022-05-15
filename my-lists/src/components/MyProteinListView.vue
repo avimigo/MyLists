92b714f4594e662259c7c5ae26f74d14c1650f1b
Migo <>
2022-05-16 00:04:21 +0900

    fix: GitHub Pages用にホームアドレス変更

--- my-lists/src/components/MyProteinListView.vue
+++ my-lists/src/components/MyProteinListView.vue
@@ -3,7 +3,7 @@
     <h1 class="text-xl font-semibold mb-4">My Protein List</h1>
     <MyProteinTable></MyProteinTable>
     <div>
-      <router-link to="/"
+      <router-link to="/MyLists"
         ><button
           class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
